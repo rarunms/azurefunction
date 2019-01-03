5aaea24cd811c736afd13a9b1f3be8e9f1c98509
Arun Radhakrishnan <rarunms@OFIMAC1013.local>
2019-01-03 16:45:28 -0500

    function name modified

--- HttpTriggerCSharp.cs
+++ HttpTriggerCSharp.cs
@@ -12,7 +12,7 @@ namespace Company.Function
 {
     public static class HttpTriggerCSharp
     {
-        [FunctionName("HttpTriggerCSharp")]
+        [FunctionName("Greetings")]
         public static async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
